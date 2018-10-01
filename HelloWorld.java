683ace9aa927394289831a67db3ee1aa318672e2
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2018-10-02 02:59:54 +0530

    🎉 Welcome to Java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld
+{   
+    static String randomString = "String to print";
+    static final double PINUM = 3.141529;
+    public static void main(String[] args)
+    {
+        System.out.println(PINUM);
+
+        int integerOne = 22;
+        int integerTwo = integerOne + 1;
+
+        System.out.println(integerTwo);
+    }
+}
