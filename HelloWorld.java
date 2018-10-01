f0943f8971884efd4b856d2704696a3de91327ec
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2018-10-02 03:14:22 +0530

    📦 Primitive data types, declaration and expression statements

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,43 @@
 public class HelloWorld
 {   
-    static String randomString = "String to print";
-    static final double PINUM = 3.141529;
     public static void main(String[] args)
     {
-        System.out.println(PINUM);
+        byte bigByte = 127;
+        short bigShort = 32767;
+        int bigInt = 2100000000;
+        long bigLong = 923414123412414141L;
+        float bigFloat = 3.14F;
+        double bigDouble = 3.1434322342424;
 
-        int integerOne = 22;
-        int integerTwo = integerOne + 1;
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
 
-        System.out.println(integerTwo);
+        boolean trueOrFalse = true;
+
+        char randomChar = 65;
+        char anotherChar = 'A';
+        System.out.println(randomChar);
+
+        char escapedChars = '\n';
+
+        String randomString = "This is a random string";
+        String anotherString = " in Java";
+        String andAnotherString = randomString + anotherString;
+        System.out.println(andAnotherString);
+
+        String byteString = Byte.toString(bigByte);
+        String shortString = Short.toString(bigShort);
+        String intString = Integer.toString(bigInt);
+        String longString = Long.toString(bigLong);
+        String floatString = Float.toString(bigFloat);
+        String doubleString = Double.toString(bigDouble);
+
+        double doubleValue = 3.1465272627;
+        int doubleInt = (int) doubleValue;
+        System.out.println(doubleInt);
+
+        int stringToInt = Integer.parseInt(intString);
+        System.out.println(stringToInt);
+
     }
 }
